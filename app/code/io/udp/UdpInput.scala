fc703bfeda074ad52ff2fa2c687174460d2ce716
Arseny Tolmachev <eiennohito@gmail.com>
2015-11-16 23:38:18 +0900

    save some input on start

--- app/code/io/udp/UdpInput.scala
+++ app/code/io/udp/UdpInput.scala
@@ -4,7 +4,9 @@ import java.net.InetSocketAddress
 
 import akka.actor.{Terminated, ActorLogging, ActorRef, Actor}
 import akka.io.{Udp, IO}
+import akka.util.ByteString
 
+import scala.collection.mutable.ArrayBuffer
 import scala.concurrent.Promise
 
 /**
@@ -45,13 +47,22 @@ class UdpInput(hostname: String, port: Int) extends Actor with ActorLogging {
       sink = null
   }
 
+  val savedInput = new ArrayBuffer[ByteString]()
+
   def ready(socket: ActorRef): Receive = {
     case UdpInput.Register =>
       sink = sender()
+      savedInput.foreach(sink ! _)
+      savedInput.clear()
     case UdpInput.AddressQuery =>
       sender() ! boundAddress
     case Udp.Received(data, who) => //data
-      sink ! data
+      if (sink != null) {
+        sink ! data
+      } else {
+        log.warning(s"stashing input: ${savedInput.size} <+ ${data.length}")
+        savedInput += data
+      }
     case Udp.Unbind => socket ! Udp.Unbind
     case Udp.Unbound =>
       sock = null
